6931f230d529f4af1ba0af5ffd550623a9728061
ChengTian <chengtian.wang@gmail.com>
2015-08-18 07:02:06 +0000

    Add database migrations

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 print(os.environ['APP_SETTINGS'])
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
